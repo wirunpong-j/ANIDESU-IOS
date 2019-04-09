d02ce1351f265fdbba5dc3f88a378a099ddc3b6e
Wirunpong Jaingamlertwong <wirunpong.j@outlook.com>
2019-04-09 12:21:42 +0700

    create post detail class

---
+++ ANIDESU/ANIDESU/ViewController/Home/PostDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PostDetailViewController.swift
+//  ANIDESU
+//
+//  Created by Wirunpong Jaingamlertwong on 9/4/2562 BE.
+//  Copyright © 2562 Wirunpong Jaingamlertwong. All rights reserved.
+//
+
+import UIKit
+
+class PostDetailViewController: BaseViewController {
+    static let identifier = "PostDetailViewController"
+
+    @IBOutlet weak var postTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
